67d7316df197fbf1a6839d53e40f28dd67195023
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2023-10-13 23:15:59 -0400

    feat: add menu switch through css

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
+const ui = {
+  menu: null,
+  menuItems: null,
+  navItems: null,
+  hamburger: null,
+  closeIcon: null,
+  menuIcon: null,
+};
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (ui.menu.classList.contains("show-menu")) {
+    // ui.hamburger.classList.remove("show-menu");
+    ui.menu.classList.remove("show-menu");
+    ui.closeIcon.style.display = "none";
+    ui.menuIcon.style.display = "block";
   } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    // ui.hamburger.classList.add("show-menu");
+    ui.menu.classList.add("show-menu");
+    ui.closeIcon.style.display = "block";
+    ui.menuIcon.style.display = "none";
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+function main() {
+  ui.menu = document.querySelector(".menu");
+  ui.menuItems = document.querySelectorAll(".menu-item");
+  ui.navItems = document.querySelectorAll(".nav-item");
+  ui.hamburger = document.querySelector(".hamburger");
+  ui.closeIcon = document.querySelector(".close-icon");
+  ui.menuIcon = document.querySelector(".menu-icon");
 
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenu);
-  }
-)
+  ui.hamburger.addEventListener("click", toggleMenu);
+
+  ui.menuItems.forEach(
+    function (menuItem) {
+      menuItem.addEventListener("click", toggleMenu);
+    }
+  );
+
+  // window.addEventListener("resize", function () {
+  //   var w = window.innerWidth;
+  //   var tabletAndUp = 768;
+  //   console.log(w)
+
+  //   if (w < tabletAndUp) {
+  //     ui.menu.classList.add("menu");
+  //     ui.menu.classList.remove("nav-wrapper");
+  //     ui.navItems.forEach((navItem) => {
+  //       navItem.classList.add("nav-item");
+  //       navItem.classList.remove("menuItem");
+  //     });
+  //   } else {
+  //     ui.menu.classList.remove("menu");
+  //     ui.menu.classList.add("nav-wrapper");
+  //   }
+  // });
+}
+
+main();
